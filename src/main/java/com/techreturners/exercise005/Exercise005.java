4d61927b11df679deeab5af7de1f1c5848bb9321
Aykoda <aykodas@gmail.com>
2021-12-11 14:34:50 +0200

    11.12.2021 Version

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -8,7 +8,29 @@ import java.util.Set;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        int abcENG = 26;
+
+        //"The quick brown fox jumps overrr the lazyyy   doggggg"
+        //eliminate all empty chars
+        input = (input.toLowerCase(new Locale("en", "US"))).replaceAll("\\s+", "");
+
+        if (!isAlpha(input)) return false;
+
+        //split each char into array
+        String[] str = input.split("(?!^)");
+
+        //group by the multiple chars by HashSet
+        Set result = new HashSet(Arrays.asList(str));
+
+        if (result.size()!= abcENG)
+            return false;
+
+        return true;
+    }
+
+    public static boolean isAlpha(String name) {
+        return name.matches("[a-z]+");
     }
 
 }
